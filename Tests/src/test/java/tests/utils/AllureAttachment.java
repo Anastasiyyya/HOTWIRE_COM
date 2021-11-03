99ab5379a411cced3e3b56aaa8a050bb1bdd6d74
AnastasiyaShevtsova <anastasiya.shevtsova.jazzteam@gmail.com>
2021-11-03 11:34:15 +0300

    edit new functions for take a screenshot after failed tests

--- Tests/src/test/java/tests/utils/AllureAttachment.java
+++ Tests/src/test/java/tests/utils/AllureAttachment.java
@@ -5,20 +5,21 @@ import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Allure;
 import io.qameta.allure.AllureLifecycle;
 import io.qameta.allure.selenide.LogType;
-import jdk.internal.org.jline.utils.Log;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.logging.Level;
 
+@Log4j2
 public class AllureAttachment {
 
     public static Optional<byte[]> getScreenshot() {
         try {
             return WebDriverRunner.hasWebDriverStarted() ? Optional.of(((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES)) : Optional.empty();
         } catch (Exception ex) {
-            Log.error("Could not get page screenshot!\n" + ex.getMessage());
+            log.error("Could not get page screenshot!\n" + ex.getMessage());
             return Optional.empty();
         }
     }
@@ -27,7 +28,7 @@ public class AllureAttachment {
         try {
             return WebDriverRunner.hasWebDriverStarted() ? Optional.of(String.join("\n", Selenide.getWebDriverLogs(LogType.BROWSER.toString(), Level.ALL)).getBytes(StandardCharsets.UTF_8)) : Optional.empty();
         } catch (Exception ex) {
-            Log.error("Could not get browser console log!\n" + ex.getMessage());
+            log.error("Could not get browser console log!\n" + ex.getMessage());
             return Optional.empty();
         }
     }
